f92ec77d3f6f2e6f0c66eb2beb01b64b9db6da4e
sougata <sougataghar47@gmail.com>
2025-04-12 20:42:17 +0530

    changed vite config

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,7 @@
 import {  useEffect } from "react"
 export default function Home(){
     useEffect(()=>{
-        fetch('https://back-nine-zeta.vercel.app/home', {
-            credentials: 'include'
-          })
+        fetch('https://back-nine-zeta.vercel.app/home')
         .then(r => r.json())
         .then(a => console.log(a))
     },[])
