36e4a2b3c7d3ebcad09d85543f578a9ba7882179
sougata <sougataghar47@gmail.com>
2025-04-12 22:30:58 +0530

    changed backend url

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,8 @@ import {  useEffect } from "react"
 export default function Home(){
     useEffect(()=>{
         fetch('https://mern-back-production-aab8.up.railway.app/home', {
-            credentials: 'include'
+            credentials: 'include',
+            mode: 'cors',
           })
         .then(r => r.json())
         .then(a => console.log(a))
